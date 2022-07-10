9c0c751776a24ef09b332c43670b288e80a94d8c
mc-Vvp <maximvaisman2801@gmail.com>
2022-07-10 23:34:56 +0300

    cards now get content from json server

--- js/practiceFood.js
+++ js/practiceFood.js
@@ -185,36 +185,42 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    new menuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        ".menu .container",
-    ).render();
+    const getResources = async (url) => {
+        const res = await fetch(url);
+        if(!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`); //trow error if request is mot ok (because fetch only vount error if no connection)
+        }
+        return await res.json();
+    };
 
-    new menuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        14,
-        ".menu .container",
-        "menu__item"
-    ).render();
+    getResources("http://localhost:3000/menu")
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new menuCard(img, altimg, title, descr, price, ".menu .container").render();
+            });
+        });
 
-    new menuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        21,
-        ".menu .container",
-        "menu__item"
-    ).render();
+       /*  getResources("http://localhost:3000/menu")
+            .then(data => createCard(data));
+        function createCard(data) {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                const element = document.createElement("div");
+                element.classList.add("menu__item");
+                element.innerHTML = `
+                    <img src=${img} alt=${altimg}>
+                    <h3 class="menu__item-subtitle">${title}</h3>
+                    <div class="menu__item-descr">${descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${price}</span> нис/день</div>
+                    </div>
+                `;
+                document.querySelector(".menu .container").append(element);
+            });
+        } */  // another way
 
-    
+
     // SEND DATA TO SERVER (forms)
     const forms = document.querySelectorAll("form");
 
@@ -237,7 +243,7 @@ window.addEventListener("DOMContentLoaded", () => {
             body: data
         });
 
-        return res.json();
+        return await res.json();
     };
 
     function bindPostData(form) {
