49929398b5010b57376c4f59edb17eec9ce4bb13
mc-Vvp <maximvaisman2801@gmail.com>
2022-07-10 23:47:37 +0300

    cards get data from json server with axios

--- js/practiceFood.js
+++ js/practiceFood.js
@@ -193,32 +193,19 @@ window.addEventListener("DOMContentLoaded", () => {
         return await res.json();
     };
 
-    getResources("http://localhost:3000/menu")
+    /* getResources("http://localhost:3000/menu")
         .then(data => {
             data.forEach(({img, altimg, title, descr, price}) => {
                 new menuCard(img, altimg, title, descr, price, ".menu .container").render();
             });
-        });
+        }); */
 
-       /*  getResources("http://localhost:3000/menu")
-            .then(data => createCard(data));
-        function createCard(data) {
-            data.forEach(({img, altimg, title, descr, price}) => {
-                const element = document.createElement("div");
-                element.classList.add("menu__item");
-                element.innerHTML = `
-                    <img src=${img} alt=${altimg}>
-                    <h3 class="menu__item-subtitle">${title}</h3>
-                    <div class="menu__item-descr">${descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${price}</span> нис/день</div>
-                    </div>
-                `;
-                document.querySelector(".menu .container").append(element);
+       axios.get("http://localhost:3000/menu")
+            .then(data => {
+                data.data.forEach(({img, altimg, title, descr, price}) => {
+                    new menuCard(img, altimg, title, descr, price, ".menu .container").render();
+                });
             });
-        } */  // another way
 
 
     // SEND DATA TO SERVER (forms)
