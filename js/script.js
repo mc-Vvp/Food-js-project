21bdb2ba52f3f2db2ed8b60d34f79ada48467532
mc-Vvp <maximvaisman2801@gmail.com>
2022-07-16 11:48:36 +0300

    changed to es6 + changed modules to use given id's

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const tabs = require("./modules/tabs"),
-          modal = require("./modules/modal"),
-          timer = require("./modules/timer"),
-          cards = require("./modules/cards"),
-          calculator = require("./modules/calculator"),
-          forms = require("./modules/forms"),
-          slider = require("./modules/slider");
+import tabs from "./modules/tabs";
+import modal from "./modules/modal";
+import timer from "./modules/timer";
+import cards from "./modules/cards";
+import calculator from "./modules/calculator";
+import forms from "./modules/forms";
+import slider from "./modules/slider";
+import {openModal} from "./modules/modal";
 
-    tabs();
-    modal();
-    timer();
+window.addEventListener("DOMContentLoaded", () => {
+
+    const modalTimer = setTimeout(() => openModal(".modal", modalTimer), 50000);
+
+    tabs(".tabheader__item", ".tabcontent", ".tabheader__items", "tabheader__item_active");
+    modal("[data-modal]", ".modal", modalTimer);
+    timer(".timer", "2022-09-22 00:00");
     cards();
     calculator();
-    forms();
-    slider();
+    forms("form", modalTimer);
+    slider({
+        container: ".offer__slider",
+        nextArrow: ".offer__slider-next",
+        slide: ".offer__slide",
+        prewArrow: ".offer__slider-prev",
+        totalCounter: "total",
+        currentCounter: "current",
+        wrapper: ".offer__slider-wrapper",
+        field: ".offer__slider-inner"
+    });
 });
